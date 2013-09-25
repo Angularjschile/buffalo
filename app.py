22428506282885ff1ab9a6dfd90ad9c1531b2d01
Alfred Chiesa <achiesa@riotgames.com>
2013-09-25 15:51:35 -0700

    added some basic auth into this thing

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ from functools import wraps
 DO NOT MESS WITH THIS STUFF
 """
 # some basic http auth for now, use the @protected decorator on all public views
-def _check_creds(username, password):
-	return username == 'admin' and password == 'purple'
+def check_auth(username, password):
+    return username == 'admin' and password == 'juicy'
 
-def _bad_creds():
-	return Response(
-		'Could not verify your access level for that URL.\n'
-		'You have to login with proper credentials dawg', 401,
-		{'WWW-Authenticate': 'Basic realm="Login Required"'})
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'I needs them proper credentials amigo.', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-def protected(c):
-	@wraps(c)
-	def decorated(*args, **kwargs):
-		auth = request.authorization
-		if not auth and not _check_creds(auth.username, auth.password):
-			return _bad_creds()
-		return c(*args, **kwargs)
-	return decorated
+def protect(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 """
 FEEL FREE TO MESS WITH THIS STUFF
@@ -30,6 +30,7 @@ FEEL FREE TO MESS WITH THIS STUFF
 app = Flask(__name__)
 
 @app.route('/')
+@protect
 def index():
     return render_template('index.html')
 
